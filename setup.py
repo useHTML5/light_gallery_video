faef4d86910fe8d58f61fab27f171f57ba33a034
useHTML5 <mne@sait-nado.ru>
2020-02-11 17:54:35 +0500

    init

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="light_gallery_video",
     version="0.3",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "django>=2.2",
         'django-cms>=3.5',
